9646dc1b6349b9365bb5e4c50c030916300b9dfd
gil56 <gil@aiola.com>
2023-02-23 20:23:34 +0200

    fixing the path of the data

--- src/models/classification_model.py
+++ src/models/classification_model.py
@@ -1,12 +1,10 @@
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 
-# Load the dataset
-data = pd.read_csv('/Users/gilayache/PycharmProjects/Final-project-feature-selection-in-gene-expression/data/processed/merged_dataset.csv')
-
+#Load the dataset
+data = pd.read_csv('../../data/processed/merged_dataset.csv')
 # Preprocessing the data
 # need to change this one.#
 X = data.iloc[:, 1:-6]# Dropping irrelevant columns (keeping only the gene expression data)
