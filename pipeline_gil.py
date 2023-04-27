5feb81f3aa13b7a957b9c97eae77444d5369873a
noaleo <noaleonard93@gmail.com>
2023-04-27 20:54:35 +0300

    removed todo

--- pipeline_gil.py
+++ pipeline_gil.py
@@ -26,7 +26,6 @@ SEED = 42
 
 columns_to_remove = ["ER", "Lympho", "samplename"]
 
-# todo: ask gil what does he mean by the below comment
 # need to change this so the pipeline will apply this ('imputer', Imputer(NUMERICAL, CATEGORICAL, num_method='mean', cat_method='most_frequent')
 # and not just remove columns
 columns_with_missing_values = df.columns[df.isnull().any()]
@@ -167,7 +166,3 @@ y_test_pred = pipe.predict(X_test)
 # Evaluate the pipeline on the test data
 mse = mean_squared_error(y_test, y_test_pred)
 print("Mean Squared Error:", mse)
-
-# todo Noa --> ask gil first:
-# 1. before we do pipe fit, we need to do some preprocessing steps (read data, split into x,y, train test split) -->
-# lets look together at run_pipeline, i think we can combine them
