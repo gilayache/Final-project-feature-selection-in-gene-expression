b3d57121f68d39be9d0a0bc2b579c557bcd5cbfc
gilayache <gil@aiola.com>
2023-05-06 20:36:36 +0300

    update the feature selection.py

--- src/feature_selection/features_selection.py
+++ src/feature_selection/features_selection.py
@@ -5,34 +5,31 @@ from sklearn.linear_model import ElasticNet
 from sklearn.linear_model import LogisticRegression
 from typing import List
 
-class FeatureSelection:
+class FeaturesSelection:
     """
     This class is responsible for feature selection. Each method returns the selected features
     """
 
-    def __init__(
-        self,
-        model_type: str,
-        K: int,
-        random_state: int,
-        alpha: float,
-        l1_ratio: float,
-        C: float,
-    ):
+    def __init__(self, fs_method: str, model_type: str, K: int = 5, random_state: int = 42, alpha: float = 0.01,
+                 l1_ratio: float = 0.5, C: float = 0.01):
         """
-                :param model_type: `regression` or `classification`
-                :param K: num of cols to choose for mrmr.
-                :param random_state.
-                :param alpha: is a regularization parameter that controls the strength of the penalty applied to the coefficients for elastic net.
-        :       :param l1_ratio: controls the mix of L1 and L2 penalties in the Elastic Net regularization.
-                :param C: Is the inverse of regularization strength for the classification problem for elastic net.
+        :param fs_method: the method of the feature selection (elastic_net or mrr)
+        :param model_type: `regression` or `classification`
+        :param K: num of cols to choose for mrmr.
+        :param random_state.
+        :param alpha: is a regularization parameter that controls the strength of the penalty applied to the coefficients for elastic net.
+        :param l1_ratio: controls the mix of L1 and L2 penalties in the Elastic Net regularization.
+        :param C: Is the inverse of regularization strength for the classification problem for elastic net.
+        :param List of the names of the selected features
         """
+        self.fs_method = fs_method
         self.model_type = model_type
         self.K = K
         self.random_state = random_state
         self.alpha = alpha
         self.l1_ratio = l1_ratio
         self.C = C
+        self.selected_features = []
 
     def mrmr(self, X: pd.DataFrame, y: pd.Series) -> List:
         """
@@ -86,6 +83,24 @@ class FeatureSelection:
         selected_features = X.columns[best_feature_indices]
         return selected_features
 
+    def fit(self, X:pd.DataFrame, y:pd.Series):
+        """
+        Applying the feature selection method and return the all the feature selection parameters
+        including the features names.
+        """
+        if self.fs_method == "mrmr":
+            self.selected_features = self.mrmr(X, y=y)
+
+        elif self.fs_method == "elastic_net":
+            self.selected_features = self.elastic_net(X, y=y)
+        return self
+
+    def transform(self, X:pd.DataFrame):
+        """
+        Return the filtered dataframe after the feature selection applied
+        """
+        return X[self.selected_features]
+
 
 ########## for debugging only #########
 
@@ -95,7 +110,7 @@ class FeatureSelection:
 # )
 # columns_to_remove = ["ER", "samplename"]
 # FEATURES = df.columns.drop(columns_to_remove)
-#
+# #
 # from sklearn.preprocessing import LabelEncoder
 #
 # # encode the categorical features
@@ -103,22 +118,11 @@ class FeatureSelection:
 # for column in cat_columns:
 #     label_encoder = LabelEncoder()
 #     df[column] = label_encoder.fit_transform(df[column])
-#
-#
+# #
+# #
 # X = df[FEATURES]
 # y = df["ER"]
 #
-# fs = FeatureSelection(
-#     model_type="classification", K=10, random_state=42, alpha=0.01, l1_ratio=0.5, C=0.01
-# )
-# # mrmr
-# # selected_feature_names = fs.mrmr(X, y)
-# # print(selected_feature_names)
-# # print(len(selected_feature_names))
-#
-# # Elastic net
-# # Filling missing values with 0
-# # Dropping columns with all missing values
 # cols_with_nan = X.columns[X.isna().any()].tolist()
 # [
 #     X.drop(columns=col, inplace=True)
@@ -127,7 +131,47 @@ class FeatureSelection:
 # ]
 #
 # X = X.fillna(0)
-# selected_feature_names = fs.elastic_net(X, y)
 #
-# print(selected_feature_names)
-# print(len(selected_feature_names))
+# mrmr
+# fs = FeaturesSelection(
+#     fs_method="mrmr", model_type="classification", K=10, random_state=42, alpha=0.01, l1_ratio=0.5, C=0.01
+# )
+#
+# # Fit the FeaturesSelection object on the data
+# fs.fit(X, y)
+#
+# # Transform the input data (X) using the selected features
+# transformed_X = fs.transform(X)
+#
+# # Print the transformed data
+# print(transformed_X)
+# print(len(transformed_X))
+
+
+
+
+# Elastic net
+# Filling missing values with 0
+# Dropping columns with all missing values
+# cols_with_nan = X.columns[X.isna().any()].tolist()
+# [
+#     X.drop(columns=col, inplace=True)
+#     for col in cols_with_nan
+#     if X[col].isna().sum() / X.shape[0] == 1
+# ]
+#
+# X = X.fillna(0)
+#
+# fs = FeaturesSelection(
+#     fs_method="elastic_net", model_type="classification", K=10, random_state=42, alpha=0.01, l1_ratio=0.5, C=0.01
+# )
+# # Fit the FeaturesSelection object on the data
+# fs.fit(X, y)
+#
+# # Transform the input data (X) using the selected features
+# transformed_X = fs.transform(X)
+#
+# # Print the transformed data
+# print(transformed_X)
+# print(len(transformed_X))
+
