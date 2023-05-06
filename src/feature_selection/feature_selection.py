8fd0e8e5faa29ed5f08a4d0635281759ee6b910b
gil56 <gil@aiola.com>
2023-05-06 13:10:47 +0300

    working version of the feature selection

--- src/feature_selection/feature_selection.py
+++ src/feature_selection/feature_selection.py
@@ -10,8 +10,8 @@ class FeaturesSelection:
     This class is responsible for feature selection. Each method returns the selected features
     """
 
-    def __init__(self, fs_method: str, model_type: str, K: int = 5, random_state: int = 42, alpha: float = 0.2,
-                 l1_ratio: float = 0.3, C: float = 0.4):
+    def __init__(self, fs_method: str, model_type: str, K: int = 5, random_state: int = 42, alpha: float = 0.01,
+                 l1_ratio: float = 0.5, C: float = 0.01):
         """
         :param fs_method: the method of the feature selection (elastic_net or mrr)
         :param model_type: `regression` or `classification`
@@ -92,7 +92,7 @@ class FeaturesSelection:
             self.selected_features = self.mrmr(X, y=y)
 
         elif self.fs_method == "elastic_net":
-            self.elastic_net(X, y=y)
+            self.selected_features = self.elastic_net(X, y=y)
         return self
 
     def transform(self, X:pd.DataFrame):
@@ -110,7 +110,7 @@ class FeaturesSelection:
 # )
 # columns_to_remove = ["ER", "samplename"]
 # FEATURES = df.columns.drop(columns_to_remove)
-#
+# #
 # from sklearn.preprocessing import LabelEncoder
 #
 # # encode the categorical features
@@ -118,22 +118,11 @@ class FeaturesSelection:
 # for column in cat_columns:
 #     label_encoder = LabelEncoder()
 #     df[column] = label_encoder.fit_transform(df[column])
-#
-#
+# #
+# #
 # X = df[FEATURES]
 # y = df["ER"]
 #
-# fs = FeaturesSelection(
-#     model_type="classification", K=10, random_state=42, alpha=0.01, l1_ratio=0.5, C=0.01
-# )
-# # mrmr
-# selected_feature_names = fs.mrmr(X, y)
-# print(selected_feature_names)
-# print(len(selected_feature_names))
-#
-# # Elastic net
-# # Filling missing values with 0
-# # Dropping columns with all missing values
 # cols_with_nan = X.columns[X.isna().any()].tolist()
 # [
 #     X.drop(columns=col, inplace=True)
@@ -142,7 +131,46 @@ class FeaturesSelection:
 # ]
 #
 # X = X.fillna(0)
-# selected_feature_names = fs.elastic_net(X, y)
 #
-# print(selected_feature_names)
-# print(len(selected_feature_names))
+# # mrmr
+# fs = FeaturesSelection(
+#     fs_method="elastic_net", model_type="classification", K=10, random_state=42, alpha=0.01, l1_ratio=0.5, C=0.01
+# )
+#
+# # Fit the FeaturesSelection object on the data
+# fs.fit(X, y)
+#
+# # Transform the input data (X) using the selected features
+# transformed_X = fs.transform(X)
+#
+# # Print the transformed data
+# print(transformed_X)
+# print(len(transformed_X))
+
+
+
+
+# Elastic net
+# Filling missing values with 0
+# Dropping columns with all missing values
+# cols_with_nan = X.columns[X.isna().any()].tolist()
+# [
+#     X.drop(columns=col, inplace=True)
+#     for col in cols_with_nan
+#     if X[col].isna().sum() / X.shape[0] == 1
+# ]
+#
+# X = X.fillna(0)
+#
+# fs = FeaturesSelection(
+#     fs_method="elastic_net", model_type="classification", K=10, random_state=42, alpha=0.01, l1_ratio=0.5, C=0.01
+# )
+# # Fit the FeaturesSelection object on the data
+# fs.fit(X, y)
+#
+# # Transform the input data (X) using the selected features
+# transformed_X = fs.transform(X)
+#
+# # Print the transformed data
+# print(transformed_X)
+# print(len(transformed_X))
