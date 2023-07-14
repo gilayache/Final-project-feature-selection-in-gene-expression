2bbf4199dc8392b60f5661e38b4a0716df0c8486
noa_leonard <noaleonard93@gmail.com>
2023-07-14 18:26:26 +0300

    added method apply_hyper_param - wip

--- src/models/modeling.py
+++ src/models/modeling.py
@@ -1,13 +1,19 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression, LinearRegression
-
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import uniform
+from sklearn.model_selection import train_test_split
 
 class Model:
 
-    def __init__(self, model_name: str):
+    def __init__(self, model_name: str, val_size, seed, hyper_params_dict):
         """
         """
         self.model = self.InitModel(model_name)
+        self.val_size = val_size
+        self.seed = seed
+        self.hyper_params_dict = hyper_params_dict
+
 
     def fit(self, X, y=None):
         """
@@ -18,13 +24,20 @@ class Model:
         elif type(y) == pd.Series:
             y = y.values.reshape(-1, 1)
 
-        self.model.fit(X, y)
+        # Further split train+validation into separate train and validation sets
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=self.val_size,
+                                                          random_state=self.seed)
+
+        best_params = self.apply_hyper_param(X_val, y_val)
+        print('start model fit')
+        self.model.fit(X, y, **best_params)
 
         return self
 
     def transform(self, X):
         """
         """
+        print('start model transform')
         X_transformed = self.model.transform(X)
 
         return X_transformed
@@ -47,3 +60,13 @@ class Model:
 
         return model
 
+    def apply_hyper_param(self, X, y):
+        """
+
+        """
+        clf = RandomizedSearchCV(self.model, self.hyper_params_dict, random_state=0)
+        print('start hyper params with RandomizedSearchCV')
+        search = clf.fit(X, y)
+        best_params = search.best_params_
+
+        return best_params
