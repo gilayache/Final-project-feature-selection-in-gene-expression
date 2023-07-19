b87d8748e2c5e805aaf5faf6619bcf0a1ea677d9
gilayache <gil@aiola.com>
2023-07-19 22:14:27 +0300

    arranging the pipe_v2

--- pipe_v2.py
+++ pipe_v2.py
@@ -1,19 +1,15 @@
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import mean_squared_error, classification_report
 import pandas as pd
 
+import src.Preprocessing.encoding as encoding
 import src.Preprocessing.imputation as imputation
 import src.Preprocessing.preprocesing as preprocesing
 import src.Preprocessing.scaling as scaling
-import src.Preprocessing.encoding as encoding
-import src.models.modeling as modeling
 import src.feature_selection.features_selection as features_selection
-from src.evaluation import evaluate
-
+import src.models.modeling as modeling
 import src.evaluation.evaluate as evaluation
-
 from src import Utils
-from sklearn.model_selection import train_test_split
 
 class RunPipeline:
 
@@ -22,7 +18,6 @@ class RunPipeline:
         self.df = pd.DataFrame()
         self.params_path = params_path
 
-
     def run(self):
         """
         Runs the pipeline
@@ -30,7 +25,6 @@ class RunPipeline:
         self.df, params = self.load_data_and_params()
         X, y = preprocesing.Preprocessing.create_x_y(self)
         X = preprocesing.Preprocessing.remove_nan_columns(self, X)
-
         X = preprocesing.Preprocessing.remove_constant_columns(self, X)
 
         # Split the data into train+validation and test sets
@@ -43,7 +37,6 @@ class RunPipeline:
 
         y_train, y_val, y_test = y_train.values.reshape(-1, 1), y_val.values.reshape(-1, 1), y_test.values.reshape(-1,
                                                                                                                    1)
-
         self.features = X_train.columns.to_list()
         self.numerical_features = X_train.select_dtypes("number").columns
         self.categorical_features = X_train.select_dtypes("object").columns
@@ -77,7 +70,6 @@ class RunPipeline:
             setattr(self, key, value)
 
         return df, params
-
 
 if __name__ == '__main__':
     run_pipeline = RunPipeline(params_path='src/data/params.yaml',
