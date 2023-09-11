0c37a0eba7de05fcfddc075c94578e9f788e8458
gilayache <gil@aiola.com>
2023-09-11 22:37:47 +0300

    with the 100 random iterations

--- src/evaluation/viz_datasets.py
+++ src/evaluation/viz_datasets.py
@@ -4,13 +4,11 @@ import yaml
 import numpy as np
 import plotly.io as pio
 
-
 # Load parameters
 with open('../../src/data/params.yaml', 'r') as f:
     params = yaml.safe_load(f)
 analysis_data_logistic = 'datasets_analysis_logistic_regression_mrmr.csv'
 analysis_data_linear = 'datasets_analysis_linear_regression_mrmr.csv'
-
 
 # Check model type and modify title
 if params['model_type'] == 'classification':
@@ -29,15 +27,25 @@ elif params['fs_params']['fs_method_1'] == 'genetic_selection':
     title += ' Using Genetic Selection:'
 
 # Filter data
-df_train = df[df['data_set'] == 'train']
-df_val = df[df['data_set'] == 'val']
-df_test = df[df['data_set'] == 'test']
+df_train = df[df['data_set'] == 'train'].copy()
+df_val = df[df['data_set'] == 'val'].copy()
+df_test = df[df['data_set'] == 'test'].copy()
 
 color_map = {
     'train': 'blue',
     'val': 'green',
     'test': 'red'
 }
+
+# Convert string representation of lists to actual lists (assuming lists are stored as strings)
+df_train.loc[:, "random_selection_score_list"] = df_train["random_selection_score_list"].apply(yaml.safe_load)
+df_val.loc[:, "random_selection_score_list"] = df_val["random_selection_score_list"].apply(yaml.safe_load)
+df_test.loc[:, "random_selection_score_list"] = df_test["random_selection_score_list"].apply(yaml.safe_load)
+
+# Calculate median for each list and store in a new column
+df_train.loc[:, "median_random"] = df_train["random_selection_score_list"].apply(np.median)
+df_val.loc[:, "median_random"] = df_val["random_selection_score_list"].apply(np.median)
+df_test.loc[:, "median_random"] = df_test["random_selection_score_list"].apply(np.median)
 
 # Create traces for feature_selection_score
 trace_train_fs = go.Scatter(
@@ -64,10 +72,10 @@ trace_test_fs = go.Scatter(
     name='Test FS'
 )
 
-# Create traces for random_selection_score_list
+# Create traces for median_random
 trace_train_random = go.Scatter(
     x=df_train['number_of_features'],
-    y=df_train["random_selection_score_list"],
+    y=df_train["median_random"],
     mode='lines',
     line=dict(color=color_map['train']),
     name='Train Random'
@@ -75,7 +83,7 @@ trace_train_random = go.Scatter(
 
 trace_val_random = go.Scatter(
     x=df_val['number_of_features'],
-    y=df_val["random_selection_score_list"],
+    y=df_val["median_random"],
     mode='lines',
     line=dict(color=color_map['val']),
     name='Val Random'
@@ -83,7 +91,7 @@ trace_val_random = go.Scatter(
 
 trace_test_random = go.Scatter(
     x=df_test['number_of_features'],
-    y=df_test["random_selection_score_list"],
+    y=df_test["median_random"],
     mode='lines',
     line=dict(color=color_map['test']),
     name='Test Random'
@@ -95,14 +103,11 @@ data = [trace_train_fs, trace_val_fs, trace_test_fs, trace_train_random, trace_v
 layout = go.Layout(
     title=title,
     xaxis=dict(title='Number of Features'),
-    yaxis=dict(
-        title=f'{score}'
-        # range=[np.percentile(pd.concat([df["feature_selection_score"], df["random_selection_score"]]), 2),
-        #        np.percentile(pd.concat([df["feature_selection_score"], df["random_selection_score"]]), 98)]
-        )
+    yaxis=dict(title=f'{score}')
 )
 
 # Create plot
 fig = go.Figure(data=data, layout=layout)
+
 # Write the plot to html
 pio.write_html(fig, file="linear_regression_genetic_selection.html", auto_open=True)
