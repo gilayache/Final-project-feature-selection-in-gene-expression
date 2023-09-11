aab07fa36c3f597e20b68a66e85eaa78a687ddf1
gilayache <gil@aiola.com>
2023-09-11 10:18:49 +0300

    with the 100 random iterations

--- src/evaluation/datasets_analysis.py
+++ src/evaluation/datasets_analysis.py
@@ -6,7 +6,7 @@ import src.Preprocessing.preprocesing as preprocesing
 import src.feature_selection.features_selection as features_selection
 import src.models.modeling as modeling
 
-from sklearn.metrics import mean_squared_error,f1_score
+from sklearn.metrics import mean_squared_error, f1_score
 from tqdm import tqdm
 from src import Utils
 import random
@@ -35,8 +35,8 @@ class RunPipeline:
         X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=self.val_size,
                                                           random_state=self.seed)
 
-        y_train, y_val, y_test = y_train.values.reshape(-1, 1), y_val.values.reshape(-1, 1), y_test.values.reshape(-1,
-                                                                                                                   1)
+        y_train, y_val, y_test = y_train.values.reshape(-1, 1), y_val.values.reshape(-1, 1), y_test.values.reshape(-1, 1)
+
         self.features = X_train.columns.to_list()
         self.numerical_features = X_train.select_dtypes("number").columns
         self.categorical_features = X_train.select_dtypes("object").columns
@@ -55,6 +55,8 @@ class RunPipeline:
         random.shuffle(shuffled_features)
 
         results = []
+
+        # n is the numbers of features to select for the feature selection algorithm.
         for i in tqdm(range(1, n + 1)):
             if K_value:
                 self.fs_params['K'] = i
@@ -78,43 +80,52 @@ class RunPipeline:
             preds_val = pipe.predict(X_val_filtered)
             preds_test = pipe.predict(X_test_filtered)
 
-            # so we wont select a random feature twice
-            random_features = shuffled_features[:i]
-            X_train_random = X_train[random_features]
-            X_val_random = X_val[random_features]
-            X_test_random = X_test[random_features]
+            # Create lists to collect metrics over 100 iterations
+            random_train_metrics = []
+            random_val_metrics = []
+            random_test_metrics = []
 
-            pipe_random = Pipeline(steps=[
-                ('Modeling', modeling.Model(model_name=self.model_name, val_size=self.val_size, seed=self.seed,
-                                            hyper_params_dict=self.hyper_params_dict))
-            ])
-            pipe_random.fit(X_train_random, y_train)
-            preds_train_random = pipe_random.predict(X_train_random)
-            preds_val_random = pipe_random.predict(X_val_random)
-            preds_test_random = pipe_random.predict(X_test_random)
+            # Loop for 100 iterations for random selection
+            for _ in range(100):
+                random.shuffle(shuffled_features)
 
-            if params['model_type'] == 'classification':
-                metrics_func = f1_score
-            elif params['model_type'] == 'regression':
-                metrics_func = mean_squared_error
+                # so we won't select a random feature twice in a single iteration.
+                random_features = shuffled_features[:i]
+                X_train_random = X_train[random_features]
+                X_val_random = X_val[random_features]
+                X_test_random = X_test[random_features]
 
-            for preds_fs, preds_random, y, data_set in zip([preds_train, preds_val, preds_test],
-                                                           [preds_train_random, preds_val_random, preds_test_random],
-                                                           [y_train, y_val, y_test],
-                                                           ['train', 'val', 'test']):
+                pipe_random = Pipeline(steps=[
+                    ('Modeling', modeling.Model(model_name=self.model_name, val_size=self.val_size, seed=self.seed,
+                                                hyper_params_dict=self.hyper_params_dict))
+                ])
+                pipe_random.fit(X_train_random, y_train)
+                preds_train_random = pipe_random.predict(X_train_random)
+                preds_val_random = pipe_random.predict(X_val_random)
+                preds_test_random = pipe_random.predict(X_test_random)
+
+                if params['model_type'] == 'classification':
+                    metrics_func = f1_score
+                elif params['model_type'] == 'regression':
+                    metrics_func = mean_squared_error
+
+                random_train_metrics.append(metrics_func(y_train, preds_train_random))
+                random_val_metrics.append(metrics_func(y_val, preds_val_random))
+                random_test_metrics.append(metrics_func(y_test, preds_test_random))
+
+            for preds_fs, y, random_metric_list, data_set in zip([preds_train, preds_val, preds_test], [y_train, y_val, y_test], [random_train_metrics, random_val_metrics, random_test_metrics], ['train', 'val', 'test']):
                 metrics = {
                     'feature_selection_score': metrics_func(y, preds_fs),
-                    'random_selection_score': metrics_func(y, preds_random),
+                    'random_selection_score_list': random_metric_list,
                     'number_of_features': len(current_features),
                     'data_set': data_set,
                     'model_type': model_type
                 }
                 results.append(metrics)
 
-            pd.DataFrame(results).to_csv('datasets_analysis_linear_regression_genetic_selection.csv', index=False)
+            pd.DataFrame(results).to_csv('datasets_analysis_linear_regression_mrmr.csv', index=False)
 
     def load_data_and_params(self):
-
         df = Utils.load_data(self.input_path)
         params = Utils.load_params(self.params_path)
 
@@ -122,6 +133,7 @@ class RunPipeline:
             setattr(self, key, value)
 
         return df, params
+
 
 if __name__ == '__main__':
     run_pipeline = RunPipeline(params_path='../../src/data/params.yaml',
